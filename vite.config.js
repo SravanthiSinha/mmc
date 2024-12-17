0ad26049fefb3a3687db6fe2b0fd335c07dd4bdd
SRAVANTHI SINHA <sinha.sravanthi@gmail.com>
2024-12-16 20:29:41 -0800

    fixed- gh pages refren issue

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mmc/', // Replace with your GitHub repo name
-  server: {
-    historyApiFallback: true,
-  },
+  base: '/mmc/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
   resolve: {
     alias: {
